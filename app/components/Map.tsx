d62af08c251c8171d3314e30155c62ac0b68c510
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2025-11-15 13:26:57 +0100

    fix map icon 2

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import L from "leaflet";
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+
 import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-const icon = L.icon({
+const customMarker = L.icon({
   iconUrl: markerIcon.src,
   iconRetinaUrl: markerIcon2x.src,
   shadowUrl: markerShadow.src,
@@ -19,11 +20,7 @@ interface MapProps {
   center?: number[];
 }
 
-const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const attribution =
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-
-const Map: React.FC<MapProps> = ({ center }) => {
+export default function Map({ center }: MapProps) {
   return (
     <MapContainer
       center={(center as L.LatLngExpression) || [51, -0.09]}
@@ -31,14 +28,12 @@ const Map: React.FC<MapProps> = ({ center }) => {
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
     >
-      <TileLayer url={url} attribution={attribution} />
-      {center && (
-        <Marker
-          position={center as L.LatLngExpression}
-          icon={icon} // ← icône custom obligatoire en prod
-        />
-      )}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+
+      {center && <Marker position={center as L.LatLngExpression} icon={customMarker} />}
     </MapContainer>
   );
-};
-export default Map;
+}
