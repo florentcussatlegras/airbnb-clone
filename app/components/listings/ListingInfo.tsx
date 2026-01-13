472ac8cf4955d9b6302ca1a49f1f10f7420b7d61
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-01-14 00:48:05 +0100

    put back in french

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -49,18 +49,18 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
     <div className="col-span-4 flex flex-col gap-8">
       <div className="flex flex-col gap-2">
         <div className="text-xl font-semibold flex flex-row items-center gap-2">
-          <div>Hosted by {user?.name}</div>
+          <div>Hébergé par {user?.name}</div>
           <Avatar src={user?.image} />
         </div>
         <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
             <div>
-                {guestCount} guests
+                {guestCount} invités
             </div>
             <div>
-                {roomCount} rooms
+                {roomCount} chambres
             </div>
             <div>
-                {bathroomCount} bathrooms
+                {bathroomCount} salle de bains
             </div>
         </div>
       </div>
