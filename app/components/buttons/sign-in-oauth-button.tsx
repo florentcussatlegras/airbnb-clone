64f5af8fd7ded34ae7af02cea223b3870f8294b2
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-01-13 15:54:30 +0100

    change github oauth callback url

--- app/components/buttons/sign-in-oauth-button.tsx
+++ app/components/buttons/sign-in-oauth-button.tsx
@@ -21,7 +21,7 @@ export const SignInOauthButton = ({
   async function handleClick() {
     await authClient.signIn.social({
       provider,
-      callbackURL: "/profile",
+      callbackURL: "/",
       errorCallbackURL: "/auth/sign-in/error",
       fetchOptions: {
         onRequest: () => {
