7a76d91d0b7c1378b6ca8e4b2bb34a9ae2121f21
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-01-14 01:56:57 +0100

    put back in french

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -13,7 +13,7 @@ const ListingPage = async () => {
     return (
       <ClientOnly>
         <EmptyState
-          title="Aucun favoris trouvé"
+          title="Aucun favori trouvé"
           subtitle="Il semble que vous n'avez aucune annonce favorite"
         />
       </ClientOnly>
